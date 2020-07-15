8b60695b6388e67a7f0c8a7ee2656d087b2734f5
abhi1nandy2 <nandyabhilash@gmail.com>
2020-07-16 01:35:27 +0530

    Added Chapter-3

---
+++ Code/extract_info.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import pytesseract
+import os
+import sys
+
+folder = "../Data/Chapter-{}".format(sys.argv[1])
+out_folder = "../Results/Chapter-{}".format(sys.argv[1])
+
+if os.path.exists(out_folder) == False:
+	os.mkdir(out_folder)
+
+for file in os.listdir(folder):
+	filepath = os.path.join(folder, file)
+	img = Image.open(filepath)
+	text = pytesseract.image_to_string(img)
+	out_fpath = os.path.join(out_folder, file.replace('.jpg', '.txt'))
+	with open(out_fpath, 'w') as f:
+		f.write(text)
